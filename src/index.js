bc171d2e4253420b30b25b952f808d3ddab5d01e
unknown <3334976@gmail.com>
2022-01-07 07:30:28 +0300

    feat: delete Math.sign

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 module.exports = function reverse(n) {
     if (n < 0) {
-        return (
-            parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n)
-        );
+        return parseFloat(n.toString().split("").reverse().join(""));
     } else return parseFloat(n.toString().split("").reverse().join(""));
 };
+
